75c224e281c92a3c69d9c80784e3a36fb7adab5d
Kizito M <kizomanizo@gmail.com>
2023-07-01 13:18:07 +0300

    minor updates

--- helpers/basicAuth.js
+++ helpers/basicAuth.js
@@ -1,4 +1,3 @@
-const apiHelper = require("./apiHelper");
 const basicAuth = require("express-basic-auth");
 const invalidLoginResponse = {
   status: 401,
@@ -6,12 +5,8 @@ const invalidLoginResponse = {
   title: "LOGIN",
   message: "Wrong username or password",
 };
-const noLoginInfoResponse = {
-  status: 401,
-  success: false,
-  title: "No username or password",
-};
-var checkLogin = basicAuth({
+
+const checkLogin = basicAuth({
   users: { testuser: process.env.DEMO_PASS },
   unauthorizedResponse: invalidLoginResponse,
 });
